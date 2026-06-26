6a933635a3aa690518ef0b9cdc111fffb7904bfd
Manuel Alejandro <mplaza005@gmail.com>
2026-06-26 11:03:38 -0500

    plantillas

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -1,11 +1,8 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+
+@extends('layouts.app')
+
+@section('content')
+
 <h1>Hola hijos</h1>
 
 <form action="{{route('category.store')}}" method="POST" enctype="multipart/form-data">
@@ -24,5 +21,5 @@
 <button type="submit">Enviar Formulario:</button>
 </form>
 
-</body>
-</html>
+
+@endsection
--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -1,11 +1,8 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+
+@extends('layouts.app')
+
+@section('content')
+
 <h1>formulario producto</h1>
 
 <form action="{{route('product.store')}}" method="POST" enctype="multipart/form-data">
@@ -65,9 +62,12 @@
 
 <br>
 <br><br>
-<button type="submit">Ingresar Producto:</button>
+{{-- <button type="submit">Ingresar Producto:</button> --}}
+
+<br><br>
+<button type="submit" class="btn btn-success">Ingresar Producto:</button>
+
 </form>
 
 
-</body>
-</html>
+@endsection
